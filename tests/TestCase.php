78afc2668268c7b7136a3ea340e69e45e131a1f5
ziarv <ziarv@users.noreply.github.com>
2024-03-15 07:43:08 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -55,7 +55,7 @@ class TestCase extends TestBenchTestCase
     }
 
     /**
-     * @param \Illuminate\Foundation\Application $app
+     * @param  \Illuminate\Foundation\Application  $app
      * @return void
      */
     protected function getEnvironmentSetUp($app)
